790f779ca3733e829ec14a00166b33394c153cb0
Clara Clemente <claraclementesantos@gmail.com>
2017-11-03 12:57:32 +0100

    inici

---
+++ src/swing_practica/Swing_practica.java
@@ -0,0 +1,15 @@
+package swing_practica;
+
+public class Swing_practica {
+
+    public static void main(String[] args) {
+
+        
+        Menu menu1 = new Menu();
+        // per centrar-lo a la pantalla
+        menu1.setLocationRelativeTo(null);
+        menu1.setVisible(true);
+        
+    }
+    
+}
